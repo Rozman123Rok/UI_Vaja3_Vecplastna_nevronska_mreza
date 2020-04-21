e01768dfd21a96fe5d691d256c5fbb7438a6970e
Rok Rozman <rok.rozman3@gmail.com>
2020-04-21 21:25:11 +0200

    Dodal da izberes kdaj se uci in nastavitve da vneses tako kot so v demo

--- UI_3/UI_3/Form1.Designer.cs
+++ UI_3/UI_3/Form1.Designer.cs
@@ -31,7 +31,24 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -42,10 +59,20 @@
             this.panel1.TabIndex = 0;
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel2.Controls.Add(this.numericUpDown4);
+            this.panel2.Controls.Add(this.numericUpDown3);
+            this.panel2.Controls.Add(this.numericUpDown2);
+            this.panel2.Controls.Add(this.numericUpDown1);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.panel3);
             this.panel2.Controls.Add(this.button1);
             this.panel2.Location = new System.Drawing.Point(598, -7);
             this.panel2.Name = "panel2";
@@ -62,6 +89,150 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Učenje:";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.radioButton2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.radioButton1);
+            this.panel3.Location = new System.Drawing.Point(9, 17);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(179, 33);
+            this.panel3.TabIndex = 2;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(74, 8);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(36, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ja";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(126, 8);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(39, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ne";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Stevilo vektorjev:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "St skritih nevronov: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Stopnja ucenja";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Dovoljena napaka: ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(102, 73);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(86, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(102, 115);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(86, 20);
+            this.numericUpDown2.TabIndex = 8;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 2;
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDown3.Location = new System.Drawing.Point(102, 161);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(86, 20);
+            this.numericUpDown3.TabIndex = 9;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            131072});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.DecimalPlaces = 3;
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.numericUpDown4.Location = new System.Drawing.Point(102, 212);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(86, 20);
+            this.numericUpDown4.TabIndex = 10;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            196608});
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.numericUpDown4_ValueChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -72,6 +243,13 @@
             this.Name = "Form1";
             this.Text = "Vaja 3";
             this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -81,6 +259,18 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- UI_3/UI_3/Form1.cs
+++ UI_3/UI_3/Form1.cs
@@ -12,10 +12,16 @@ namespace UI_3
 {
     public partial class Form1 : Form
     {
-        public Point nova = new Point();
-        public Point stara = new Point();
+        public Point nova = new Point(); // nova tocka pri risanju znaka
+        public Point stara = new Point(); // prejsna tocka
         public Graphics g;
         public Pen pen = new Pen(Color.Black, 5);
+        public List<Point> vektor = new List<Point>(); // tu imamo shranjene vse tocke
+        bool poteka_ucenje = true; // ce se se program uci ali ne
+        public int stevilo_vektorjev = 16; // koliko vektorjev bomo uporabili
+        public int stevilo_nevronov = 12; // koliko nevronov
+        public double stopnja_ucenja = 0.25; // stopnja ucenja
+        public double dovoljena_napaka = 0.005; // napaka
         public Form1()
         {
             InitializeComponent();
@@ -25,23 +31,79 @@ namespace UI_3
 
         private void panel1_MouseDown(object sender, MouseEventArgs e)
         {
-            stara = e.Location;
+            panel1.Invalidate(); // pocistimo 
+            stara = e.Location; // si shranimo lokacijo kjer smo zaceli oz stisnili
+            vektor.Add(stara); // dodamo v vektor
         }
 
         private void panel1_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left) 
+            if (e.Button == MouseButtons.Left) // ce drzimo levi gumb na miski
             {
-                nova = e.Location;
-                g.DrawLine(pen, stara, nova);
-                stara = nova;
-
+                nova = e.Location; // za vsak premik dobimo novo lokavijo
+                vektor.Add(nova); // dodamo v vektor
+                g.DrawLine(pen, stara, nova); // narisemo crto med novo in staro
+                stara = nova; // si prepisemo staro
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            panel1.Invalidate();
+            panel1.Invalidate(); // pocistimo panel
+            /*
+            MessageBox.Show("Zaj bom ponovno narisel!");
+            for (int i = 0; i < vektor.Count()-1; i++) {
+                g.DrawLine(pen, vektor[i], vektor[i + 1]);
+            }
+            */
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // ko spustimo misko
+            if (poteka_ucenje) // ce poteka ucenje
+            {
+                // moramo vpisati kateri znak smo narisali
+                MessageBox.Show("Tukaj bos vpisal keri znak je"); 
+            }
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            poteka_ucenje = true; // damo da poteka ucenje
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            poteka_ucenje = false; // damo da ne poteka ucenje
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("St vektorjev: " + stevilo_vektorjev);
+            stevilo_vektorjev = (int)numericUpDown1.Value; // pridobimo vrednost
+            //MessageBox.Show("St vektorjev: " + stevilo_vektorjev);
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("St nev: " + stevilo_nevronov);
+            stevilo_nevronov = (int)numericUpDown2.Value; // pridobimo vrednost
+            //MessageBox.Show("St nev: " + stevilo_nevronov);
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("St ucenja: " + stopnja_ucenja);
+            stopnja_ucenja = (double)numericUpDown3.Value; // pridobimo vrednost
+            //MessageBox.Show("St ucenja: " + stopnja_ucenja);
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Napaka: " + dovoljena_napaka);
+            dovoljena_napaka = (double)numericUpDown4.Value; // pridobimo vrednost
+            //MessageBox.Show("Napaka: " + dovoljena_napaka);
         }
     }
 }
